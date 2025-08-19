9d12837e2ae48f282e3b958219f88365a9124fe4
bhartikhare7 <bharti.khare7@gmail.com>
2025-08-18 19:17:50 -0700

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,7 @@
+def add(a, b):
+    """Adds two numbers and returns the sum."""
+    return a + b
+
+def subtract(a, b):
+    """Subtracts two numbers and returns the difference."""
+    return a - b
